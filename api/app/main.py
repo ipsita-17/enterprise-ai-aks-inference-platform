ef05cfb565e3b8127928d593a656afc25c892567
ipsita-17 <ipsitapatra173@gmail.com>
2026-06-17 13:18:50 +0530

    feat: add prometheus monitoring

--- api/app/main.py
+++ api/app/main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
+from prometheus_client import Counter, Histogram, generate_latest
+from fastapi.responses import Response
+
 app = FastAPI()
 
 model = joblib.load("model/artifacts/model.pkl")
+
+# Metrics
+
+prediction_counter = Counter(
+    "prediction_requests_total",
+    "Total prediction requests"
+)
+
+prediction_latency = Histogram(
+    "prediction_latency_seconds",
+    "Prediction latency"
+)
 
 
 class IrisRequest(BaseModel):
@@ -15,7 +30,7 @@ class IrisRequest(BaseModel):
 
 
 @app.get("/")
-def health_check():
+def root():
     return {
         "status": "healthy",
         "service": "AI Inference API"
@@ -29,8 +44,19 @@ def health():
     }
 
 
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
+
+
 @app.post("/predict")
+@prediction_latency.time()
 def predict(data: IrisRequest):
+
+    prediction_counter.inc()
 
     prediction = model.predict([[
         data.sepal_length,
