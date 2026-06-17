ab88cdc53fb74193ef2ab1a367b679e7bb288892
ipsita-17 <ipsitapatra173@gmail.com>
2026-06-17 13:50:26 +0530

    feat: add mlflow experiment tracking

--- model/training/train.py
+++ model/training/train.py
@@ -1,7 +1,14 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
 import joblib
+import mlflow
+import mlflow.sklearn
 import os
+
+# MLflow experiment
+mlflow.set_experiment("iris-classification")
 
 # Load dataset
 iris = load_iris()
@@ -9,13 +16,41 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
-# Train model
-model = RandomForestClassifier(
-    n_estimators=100,
+# Train/Test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
     random_state=42
 )
 
-model.fit(X, y)
+with mlflow.start_run():
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    accuracy = model.score(X_test, y_test)
+
+    mlflow.log_param(
+        "n_estimators",
+        100
+    )
+
+    mlflow.log_metric(
+        "accuracy",
+        accuracy
+    )
+
+    mlflow.sklearn.log_model(
+        model,
+        "model"
+    )
+
+    print(f"Accuracy: {accuracy}")
 
 # Create artifacts folder
 os.makedirs("model/artifacts", exist_ok=True)
